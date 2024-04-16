dc6dd77d2c54a8f3f32b9cc79f4052af34b438ed
KhloWee <102633629+KhloWee@users.noreply.github.com>
2024-04-16 13:30:08 -0400

    Lab 1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Lab 1
+// CIS 199-02
+// Due: 1/23/2020
+// By: R2230
+
+// This program displays student's Grading ID, 
+// hobbies, favorite book and movie to the console.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Grading ID:     R2230");
+                   Console.WriteLine("Hobbies:        Art, playing video games, and working with technology");
+                   Console.WriteLine("Favorite Book:  Zodiac");
+                   Console.WriteLine("Favorite Movie: Guardians of the Galaxy 2");
+
+        }
+    }
+}
